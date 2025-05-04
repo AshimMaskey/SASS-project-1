a7158981826d223c0c95d74f3014b34da6f95faa
Ashim Maskey <ashimmaskey4@gmail.com>
2025-05-04 20:50:30 +0545

    added handledeletebook controller

--- controllers/book.controller.js
+++ controllers/book.controller.js
@@ -1,11 +1,11 @@
 import db from "../database/connection.js";
 
-export const handleCreateBook = async (req, res) => {
+export const handleGetBooks = async (req, res) => {
   const datas = await db.books.findAll();
   res.status(200).json({ message: "books fetched successfully", datas });
 };
 
-export const handleGetBooks = async (req, res) => {
+export const handleCreateBook = async (req, res) => {
   const { bookName, bookAuthor, bookPrice, bookGenre } = req.body;
 
   if (!bookName || !bookAuthor || !bookPrice || !bookGenre) {
@@ -17,12 +17,26 @@ export const handleGetBooks = async (req, res) => {
   res.status(201).json({ message: "books created successfully", book });
 };
 
-export const handleUpdateBook = async (req, res) => {};
-
-export const handleDeleteBook = async (req, res) => {};
-
 export const handleGetSingleBook = async (req, res) => {
   const id = req.params.id;
   const datas = await db.books.findByPk(id);
   return res.status(200).json({ message: "single book fetched", datas });
+};
+
+export const handleUpdateBook = async (req, res) => {};
+
+export const handleDeleteBook = async (req, res) => {
+  const id = req.params.id;
+
+  const deleted = await db.books.destroy({
+    where: {
+      id: id,
+    },
+  });
+
+  if (deleted === 0) {
+    return res.status(400).json({ message: "Book is not deleted" });
+  } else {
+    return res.status(200).json({ message: "Book deleted successfully" });
+  }
 };
