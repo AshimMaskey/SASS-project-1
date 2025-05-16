1560355b3ce0507ed7f1209f886a5a08f954b864
Ashim Maskey <ashimmaskey4@gmail.com>
2025-05-16 21:18:45 +0545

    added cors

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 import express from "express";
 import db from "./database/connection.js";
 import bookRoute from "./routes/book.route.js";
+import cors from "cors";
 const app = express();
 
 //middleware
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 
 //routes
 app.use("/api/books", bookRoute);
